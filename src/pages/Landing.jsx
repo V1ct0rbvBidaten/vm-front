0d7e6b619e9e5f6f7bffe6cb10e106f745492f2f
V1ct0rbvBidaten <vbustamante@bidaten.com>
2024-06-22 00:58:13 -0400

    landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -183,6 +183,34 @@ const Landing = () => {
           </Card>
         </div>
       </div>
+      <div className="flex flex-col mt-5 bg-gradient-to-br from-purple-600  to-sky-700 p-20">
+        <h1 className="text-4xl font-bold text-white mb-4 text-center">
+          Lorem ipsum
+        </h1>
+        <p className="text-white">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div className="flex flex-col mt-5 p-20">
+        <h1 className="text-4xl font-bold text-purple-500 mb-4 text-center">
+          Nosotros
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
     </>
   );
 };
