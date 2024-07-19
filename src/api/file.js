1c99a83c824e969799c6277ffa9f64cdf4ad8e65
V1ct0rbvBidaten <vbustamante@bidaten.com>
2024-07-19 09:10:16 -0400

    Api: ListFiles

--- src/api/file.js
+++ src/api/file.js
@@ -46,3 +46,32 @@ export const uploadFile = async (token, params, files) => {
     throw error;
   }
 };
+
+export const getFiles = async (token, body) => {
+  let queryParams = new URLSearchParams();
+
+  Object.entries(body).forEach(([key, value]) => {
+    if (typeof value === "string" && value.includes(",")) {
+      value.split(",").forEach((item) => {
+        if (item) queryParams.append(key, item.trim());
+      });
+    } else if (Array.isArray(value)) {
+      value.forEach((item) => {
+        if (item) queryParams.append(key, item);
+      });
+    } else if (value) {
+      queryParams.append(key, value);
+    }
+  });
+
+  const queryString = queryParams.toString();
+
+  return await axios.get(
+    `${import.meta.env.VITE_API_URL}/list-files?${queryString}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
