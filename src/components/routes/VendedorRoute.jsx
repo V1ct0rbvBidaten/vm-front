2cd3f339faf0f431c119afe6d7a3c0fb3b8778dc
V1ct0rbvBidaten <vbustamante@bidaten.com>
2024-07-19 08:32:59 -0400

    fix layout vendedor

--- src/components/routes/VendedorRoute.jsx
+++ src/components/routes/VendedorRoute.jsx
@@ -45,12 +45,12 @@ const VendedorRoute = ({ Component }) => {
       <CreateProfile user={user} />
     ) : (
       <>
-        <div className=" flex  min-h-screen ">
+        <div className=" flex  max-h-screen ">
           <div className="">
             <VendedorSideNav user={user} />
           </div>
 
-          <main className="p-7 w-full bg-stone-50">
+          <main className="p-7 w-full bg-stone-50 overflow-scroll">
             <div className="flex justify-between items-center gap-4 bg-white p-3 rounded-md shadow-md">
               <Button
                 isIconOnly
