f85eb483178fd6b49797dd0de65845115445c3fa
V1ct0rbvBidaten <vbustamante@bidaten.com>
2024-07-21 16:09:05 -0400

    Maletines Hooks

---
+++ src/hooks/useMaletines.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { getMaletines } from "../api/maletin";
+
+function useMaletines(token, body, reload) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    getMaletines(token, body)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
+  }, [body, reload]);
+
+  return { data, loading, error };
+}
+
+export default useMaletines;
